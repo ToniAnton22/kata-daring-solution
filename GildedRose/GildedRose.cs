eb9545bb81b585afb13f99f5e6d1c695649e02a2
ToniAnton22 <frincuanton@yahoo.com>
2024-06-23 20:35:33 +0100

    Add: Added checks for quality to not go above 50 and below 0 (Sulfurus items exempt). Modified the function to be easier to read

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -15,91 +15,72 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (!Items[i].Name.Contains("Aged Brie") && !Items[i].Name.Contains("Backstage passes"))
+            if (Items[i].SellIn > 0)
             {
-                if (Items[i].Quality > 0)
+                if (Items[i].Quality < 50)
                 {
-                    if (!Items[i].Name.Contains("Sulfuras") && !Items[i].Name.Contains("Conjured"))
+                    if (Items[i].Name.Contains("Aged Brie"))
                     {
-                        Items[i].Quality = Items[i].Quality - 1;
+                        Items[i].Quality = Items[i].Quality + 1;
                     }
-                    
-                    
                     if (Items[i].Name.Contains("Conjured"))
                     {
-                        Items[i].Quality = Items[i].Quality - 2;   
+                        Items[i].Quality -= 2;
+                    }
+                    if (Items[i].Name.Contains("Backstage passes"))
+                    {
+                        Items[i].Quality += 1;
+
+                        if (Items[i].SellIn < 11)
+                        {
+                            Items[i].Quality += 1;
+                        }
+                        if (Items[i].SellIn < 6)
+                        {
+                            Items[i].Quality += 1;
+                        }
+                    }
+                    if (!Items[i].Name.Contains("Aged Brie") && !Items[i].Name.Contains("Conjured") && !Items[i].Name.Contains("Backstage passes"))
+                    {
+                        Items[i].Quality -= 1;
                     }
                 }
 
             }
             else
             {
-                if (Items[i].Quality < 50)
+                if (Items[i].Name.Contains("Backstage passes"))
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name.Contains("Backstage passes"))
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-              
+                    Items[i].Quality = 0;       
+                }
+                if (!Items[i].Name.Contains("Aged Brie") && !Items[i].Name.Contains("Conjured") && !Items[i].Name.Contains("Backstage passes"))
+                {
+                    Items[i].Quality -= 2;
+                }
+                if (Items[i].Name.Contains("Conjured"))
+                {
+                    Items[i].Quality -= 4;
+                }
+                if (Items[i].Name.Contains("Aged Brie"))
+                {
+                    Items[i].Quality = Items[i].Quality + 2;
                 }
             }
-
             if (!Items[i].Name.Contains("Sulfuras"))
             {
-                Items[i].SellIn = Items[i].SellIn - 1;
+                Items[i].SellIn -= 1;
             }
-
-            if (Items[i].SellIn < 0)
+            if (Items[i].Name.Contains("Sulfuras") && (Items[i].Quality < 80 || Items[i].Quality > 80))
             {
-                if (!Items[i].Name.Contains("Aged Brie"))
-                {
-                    if (!Items[i].Name.Contains("Backstage passes"))
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (!Items[i].Name.Contains("Sulfuras") && !Items[i].Name.Contains("Conjured"))
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                            
-                            if (Items[i].Name.Contains("Conjured"))
-                            {
-                                Items[i].Quality = Items[i].Quality - 2;
-                            }   
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
+                Items[i].Quality = 80;
             }
             if (Items[i].Quality < 0)
             {
                 Items[i].Quality = 0;
+            }
+            if (Items[i].Quality > 50 && !Items[i].Name.Contains("Sulfuras"))
+            {
+                Items[i].Quality = 50;
             }
         }
     }
