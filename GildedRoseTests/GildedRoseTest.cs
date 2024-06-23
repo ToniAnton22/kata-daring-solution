62cec27c4068933fe8b813d80c74d591f0b5ced0
ToniAnton22 <frincuanton@yahoo.com>
2024-06-23 19:24:11 +0100

    Add: Setup and configurations

---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void Foo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("Conjured Mana Cake", Items[0].Name);
+    }
+}
